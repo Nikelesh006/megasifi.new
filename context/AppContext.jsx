3f715c0c00ce6181dad319e83c11a77f686069c5
Nikelesleshwaran T <nikelesh2006@gmail.com>
2025-11-14 18:31:31 +0530

    commit .env file

--- context/AppContext.jsx
+++ context/AppContext.jsx
@@ -2,7 +2,9 @@
 import { productsDummyData, userDummyData } from "@/assets/assets";
 import { useRouter } from "next/navigation";
 import { createContext, useContext, useEffect, useState } from "react";
-import { useUser } from '@clerk/nextjs';
+import { useUser, useAuth } from '@clerk/nextjs';
+import axios from 'axios';
+import toast from 'react-hot-toast';
 
 
 export const AppContext = createContext();
@@ -17,18 +19,41 @@ export const AppContextProvider = (props) => {
     const router = useRouter()
 
     const { user } = useUser()
+    const { getToken }= useAuth()
 
     const [products, setProducts] = useState([])
     const [userData, setUserData] = useState(false)
-    const [isSeller, setIsSeller] = useState(true)
+    const [isSeller, setIsSeller] = useState(false)
     const [cartItems, setCartItems] = useState({})
 
     const fetchProductData = async () => {
+        
         setProducts(productsDummyData)
     }
 
     const fetchUserData = async () => {
-        setUserData(userDummyData)
+        try{
+
+           if(user.publicMetadata.role === 'seller'){
+              setIsSeller(true)
+           }
+
+           const token = await getToken()
+
+           const { data } = await axios.get('/api/user/data', { headers: { Authorization: `Bearer ${token}` } })
+
+           if(data.success){
+            setUserData(data.user)
+            setCartItems(data.data.cartItems)
+
+           }else{
+            toast.error(data.message)
+           }
+         
+        
+        }catch(error){
+            toast.error(error.message)
+        }
     }
 
     const addToCart = async (itemId) => {
@@ -82,11 +107,13 @@ export const AppContextProvider = (props) => {
     }, [])
 
     useEffect(() => {
+        if(user){
         fetchUserData()
-    }, [])
+        }
+    }, [user])
 
     const value = {
-        user,
+        user,getToken,
         currency, router,
         isSeller, setIsSeller,
         userData, fetchUserData,
