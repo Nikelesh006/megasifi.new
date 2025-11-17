5901da9115d6543edd8f1fb163becd29eb2c45cd
Nikelesleshwaran T <nikelesh2006@gmail.com>
2025-11-17 16:22:33 +0530

    Fix Inngest env and route path

--- app/api/product/list/route.js
+++ app/api/product/list/route.js
@@ -1,20 +1,16 @@
-
-import { getAuth } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 import Product from "@/models/Product";
-import { connectDB } from "@/config/db";
-
+import connectDB from "@/config/db";
 
 export async function GET(request){
-
   try{
     await connectDB()
 
-        const products=await Product.find({userId})
+    const products = await Product.find({})
 
-        return NextResponse.json({success:true,data:products})
+    return NextResponse.json({success:true,data:products})
 
-    }catch(error){
-        return NextResponse.json({success:false,message:error.message})
-    }
+  }catch(error){
+    return NextResponse.json({success:false,message:error.message})
+  }
 }
