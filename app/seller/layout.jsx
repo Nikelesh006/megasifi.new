2f90c7ff5c0e9561dd17a254ae2623ac2ed54dfa
Nikelesleshwaran T <nikelesh2006@gmail.com>
2025-11-23 01:07:14 +0530

    commit the changes done

--- app/seller/layout.jsx
+++ app/seller/layout.jsx
@@ -5,11 +5,18 @@ import React from 'react'
 
 const Layout = ({ children }) => {
   return (
-    <div>
+    <div className="min-h-screen flex flex-col">
       <Navbar />
-      <div className='flex w-full'>
-        <Sidebar />
-        {children}
+      <div className="flex w-full flex-1">
+        {/* Sidebar: fixed width + full height */}
+        <div className="w-64">
+          <Sidebar />
+        </div>
+
+        {/* Main content: flexible, padded, scrollable */}
+        <main className="flex-1 p-8 overflow-auto">
+          {children}
+        </main>
       </div>
     </div>
   )
